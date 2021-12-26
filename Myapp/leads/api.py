c928c0cd3050b5993c2bf6c3e4ffb50e155a048d
Moginder <mogiegan@gmail.com>
2021-12-26 22:39:11 +0530

    Add files via upload

---
+++ Myapp/leads/api.py
@@ -0,0 +1,10 @@
+from leads.models import Lead
+from rest_framework import viewsets, permissions
+from. serializers import LeadSerializer
+
+# Lead Viewset
+
+class LeadViewSet(viewsets.ModelViewSet):
+    queryset = Lead.objects.all()
+    permission_classes = [Permissions.AllowAny]
+    serilizer_class = LeadSerializer
