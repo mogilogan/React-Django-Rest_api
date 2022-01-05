e4b6c950f0f021f6f63bda25cfdcedbadd8225c1
Moginder <mogiegan@gmail.com>
2022-01-05 13:04:01 +0530

    Update urls.py

--- Myapp/Myapp/urls.py
+++ Myapp/Myapp/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('leads.urls')),
+    path('', include('Reactapp.urls')),
+    path('', include('leads.urls'))
 ]
