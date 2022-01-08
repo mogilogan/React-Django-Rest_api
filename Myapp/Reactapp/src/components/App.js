bc401948ef63d55932f0bc2586f7eb516278a9a1
Moginder <mogiegan@gmail.com>
2022-01-08 21:58:27 +0530

    Update App.js

--- Myapp/Reactapp/src/components/App.js
+++ Myapp/Reactapp/src/components/App.js
@@ -1,11 +1,17 @@
-import React,{ Component } from 'react';
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
-import Header from './layout/Header.js';
+import Header  from './layout/Header.js';
 
 class App extends Component {
     render(){
-        <Header/>
-    }
+        
+      
+        return (
+            <Header/>
+        
+        );  
+
+      }
 }
 
-ReactDOM.render(<App/>,document.getElementById('app'));
+ReactDOM.render(<App />,document.getElementById('app'));
